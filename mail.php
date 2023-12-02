af5f08f455763596661de1f3abdd3b8b71e9d051
Bmis Kemgoum <bmisarts@gmail.com>
2023-12-03 00:20:24 +0100

    project init

--- mail.php
+++ mail.php
@@ -18,7 +18,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "kaiumhossainrian@gmail.com";
+        $recipient = "gilleskemgoum@gmail.com";
 
         //Set the email subject.
         $subject = "New contact from $name";
