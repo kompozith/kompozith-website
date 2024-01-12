c8fefc5aad4721c699fe1ef1db3c0dcf14968032
Bmis Kemgoum <bmisarts@gmail.com>
2024-01-12 02:57:53 +0100

    portfolio

--- src/app/components/home/sections/portfolio/portfolio.component.ts
+++ src/app/components/home/sections/portfolio/portfolio.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import SwiperCore, { Navigation, Pagination, SwiperOptions, Swiper, Autoplay } from 'swiper';
+Swiper.use([Navigation, Pagination, Autoplay]);
+
+// install Swiper modules
+SwiperCore.use([Pagination]);
 
 @Component({
   selector: 'app-portfolio',
@@ -6,5 +11,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./portfolio.component.scss']
 })
 export class PortfolioComponent {
+  config1: SwiperOptions = {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    speed: 100000,
+    loop: true,
+    parallax: true,
+    autoplay: {
+      delay: 1,
+      disableOnInteraction: false,
+    },
+    freeMode: true
+  };
+  config2: SwiperOptions = {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    speed: 200000,
+    loop: true,
+    parallax: true,
+    autoplay: {
+      delay: 1,
+      disableOnInteraction: false,
+      reverseDirection: true
+    },
+    freeMode: true
+  };
+  config3: SwiperOptions = {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    speed: 300000,
+    loop: true,
+    parallax: true,
+    autoplay: {
+      delay: 1,
+      disableOnInteraction: false,
+    },
+    freeMode: true
+  };
+  config4: SwiperOptions = {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    speed: 50000,
+    loop: true,
+    parallax: true,
+    autoplay: {
+      delay: 1,
+      disableOnInteraction: false,
+      reverseDirection: true
+    },
+    freeMode: true
+  };
 
 }
