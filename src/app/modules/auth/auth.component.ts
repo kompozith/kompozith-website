37e6e403760b8596dfc20a5caa8d0e137ab17152
Bmis Kemgoum <bmisarts@gmail.com>
2024-06-23 13:17:59 +0100

    Auth login started

--- src/app/modules/auth/auth.component.ts
+++ src/app/modules/auth/auth.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.scss'
+  styleUrl: './auth.component.scss',
+  imports:[RouterOutlet]
 })
 export class AuthComponent {
 
---
+++ src/app/modules/auth/login/login.component.ts
@@ -0,0 +1,16 @@
+import { NgStyle } from '@angular/common';
+import { Component } from '@angular/core';
+import { ContainerComponent, RowComponent, ColComponent, CardGroupComponent, TextColorDirective, CardComponent, CardBodyComponent, FormDirective, InputGroupComponent, InputGroupTextDirective, FormControlDirective, ButtonDirective } from '@coreui/angular';
+import { IconDirective } from '@coreui/icons-angular';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+  imports: [ContainerComponent, RowComponent, ColComponent, CardGroupComponent, TextColorDirective, CardComponent, CardBodyComponent, FormDirective, InputGroupComponent, InputGroupTextDirective, IconDirective, FormControlDirective, ButtonDirective, NgStyle]
+
+})
+export class LoginComponent {
+
+}
